032762ef15d581cbc76eaa3a28d8da7f29ca4f87
Raul <raulgcollazo1996@gmail.com>
2017-01-09 14:05:23 +0100

    form of game semi-completed-semi-completed

--- CapaPresentacion/FrmInicio.cs
+++ CapaPresentacion/FrmInicio.cs
@@ -25,10 +25,31 @@ namespace CapaPresentacion
             lblPregunta.Text = (String) pregunta.DESCRIPCION;
             List<RESPUESTA> preguntas = new List<RESPUESTA>();
             preguntas = negocio.returnAnswersFromQuestion(pregunta.ID);
-            foreach ( RESPUESTA pre in preguntas)
-            {
 
-            }
+            Btnjuego1.Text = preguntas[0].DESCRIPCION;
+            Btnjuego1.Tag = preguntas[0].IDPREGUNTA;
+            Btnjuego2.Text = preguntas[1].DESCRIPCION;
+            Btnjuego2.Tag = preguntas[1].IDPREGUNTA;
+            Btnjuego3.Text = preguntas[2].DESCRIPCION;
+            Btnjuego3.Tag = preguntas[2].IDPREGUNTA;
+            Btnjuego4.Text = preguntas[3].DESCRIPCION;
+            Btnjuego4.Tag = preguntas[3].IDPREGUNTA;
+            Btnjuego5.Text = preguntas[4].DESCRIPCION;
+            Btnjuego5.Tag = preguntas[4].IDPREGUNTA;
+            Btnjuego6.Text = preguntas[5].DESCRIPCION;
+            Btnjuego6.Tag = preguntas[5].IDPREGUNTA;
+            Btnjuego7.Text = preguntas[6].DESCRIPCION;
+            Btnjuego7.Tag = preguntas[6].IDPREGUNTA;
+            Btnjuego8.Text = preguntas[7].DESCRIPCION;
+            Btnjuego8.Tag = preguntas[7].IDPREGUNTA;
+            Btnjuego9.Text = preguntas[8].DESCRIPCION;
+            Btnjuego9.Tag = preguntas[8].IDPREGUNTA;
+            Btnjuego10.Text = preguntas[9].DESCRIPCION;
+            Btnjuego10.Tag = preguntas[9].IDPREGUNTA;
+            Btnjuego11.Text = preguntas[10].DESCRIPCION;
+            Btnjuego11.Tag = preguntas[10].IDPREGUNTA;
+            Btnjuego12.Text = preguntas[11].DESCRIPCION;
+            Btnjuego12.Tag = preguntas[11].IDPREGUNTA;
             //respuestas
 
         }
